16e8a46c82f6cbeb5bc2820d138798d1c0ef4f3a
Ararat Mendez <ararat.mendez@gmail.com>
2017-09-13 21:37:26 -0600

    Se agrega suma y resta

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/suma", (req, res) -> {
+            return "5 + 6 = " + (5+6);
+        });
+        get("/resta", (req, res) -> {
+            return "5 - 6 = " + (5-6);
+        });
     }
 }
